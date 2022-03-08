9310cedc1af011ce3824e0171fe43f61598cde88
liveforone <yc4852@gmail.com>
2022-03-08 17:52:52 +0900

    Good commit

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -11,8 +11,8 @@ public class Item {
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
     private Long id;
-
     private String name;
+
     private int price;
     private int stockQuantity;
 
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -11,43 +13,38 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    public Long getId() {
-        return id;
-    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }  //양방향 연관관계를 위한 편의 메소드
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() {   return id;  }
 
-    public Long getMemberId() {
-        return memberId;
-    }
+    public void setId(Long id) {    this.id = id;   }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
+    public Member getMember() { return member;  }
 
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
+    public void setMember(Member member) {  this.member = member;   }
 
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
+    public LocalDateTime getOrderDate() {   return orderDate;   }
 
-    public OrderStatus getStatus() {
-        return status;
-    }
+    public void setOrderDate(LocalDateTime orderDate) { this.orderDate = orderDate; }
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
+    public OrderStatus getStatus() {    return status;  }
+
+    public void setStatus(OrderStatus status) { this.status = status;   }
+
+
 }
